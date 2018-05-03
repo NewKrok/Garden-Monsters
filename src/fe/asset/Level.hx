b82352a7f0d79b39e999179e9c11bafbb5d0702c
Krisztian Somoracz <krisztian.somoracz@gmail.com>
2018-05-03 23:21:52 +0200

    4 new levels added
    4 new levels added

--- src/fe/asset/Level.hx
+++ src/fe/asset/Level.hx
@@ -12,7 +12,7 @@ class Level
 	[
 		// LV 1 =====================================
 		{
-			maxMovement: 10,
+			maxMovement: 12,
 			availableElemTypes: [
 				ElemType.Elem8,
 				ElemType.Elem9,
@@ -64,7 +64,7 @@ class Level
 				6000
 			],
 			elemGoals: [
-				ElemType.Elem1 => 15,
+				ElemType.Elem1 => 10,
 				ElemType.Elem8 => 10,
 				ElemType.Elem11 => 10,
 			]
@@ -96,9 +96,139 @@ class Level
 				6000
 			],
 			elemGoals: [
-				ElemType.Elem1 => 15,
+				ElemType.Elem1 => 10,
 				ElemType.Elem10 => 10,
 				ElemType.Elem14 => 10,
+			]
+		},
+
+		// LV 4 =====================================
+		{
+			maxMovement: 10,
+			availableElemTypes: [
+				ElemType.Elem1,
+				ElemType.Elem8,
+				ElemType.Elem9,
+				ElemType.Elem10,
+				ElemType.Elem11,
+				ElemType.Elem14
+			],
+			rawMap: [
+				[ -2, -2, -2, -2, -2, -2, -2 ],
+				[ -2, -2, -2, -2, -2, -2, -2 ],
+				[ -2, -2, -2,  0, -2, -2, -2 ],
+				[ -2, -2,  0, -3,  0, -2, -2 ],
+				[ -2, -2,  0, -3,  0, -2, -2 ],
+				[ -2, -2, -2,  0, -2, -2, -2 ],
+				[ -2, -2, -2, -2, -2, -2, -2 ],
+				[ -2, -2, -2, -2, -2, -2, -2 ]
+			],
+			starRequirements: [
+				3000,
+				5000,
+				6000
+			],
+			elemGoals: [
+				ElemType.Elem8 => 6,
+				ElemType.Elem9 => 6,
+				ElemType.Elem11 => 6,
+				ElemType.Elem14 => 6,
+			]
+		},
+
+		// LV 5 =====================================
+		{
+			maxMovement: 15,
+			availableElemTypes: [
+				ElemType.Elem2,
+				ElemType.Elem8,
+				ElemType.Elem9,
+				ElemType.Elem10,
+				ElemType.Elem11
+			],
+			rawMap: [
+				[ -2, -2, -2, -3, -2, -2, -2 ],
+				[ -2, -3, -2, -2, -2, -3, -2 ],
+				[ -2, -2, -2, -2, -2, -2, -2 ],
+				[ -2, -2, -2, -2, -2, -2, -2 ],
+				[ -2, -2, -2, -2, -2, -2, -2 ],
+				[ -2, -2, -2, -2, -2, -2, -2 ],
+				[ -2, -3, -2, -2, -2, -3, -2 ],
+				[ -2, -2, -2, -3, -2, -2, -2 ]
+			],
+			starRequirements: [
+				3000,
+				5000,
+				6000
+			],
+			elemGoals: [
+				ElemType.Elem10 => 10,
+				ElemType.Elem11 => 20,
+			]
+		},
+
+		// LV 6 =====================================
+		{
+			maxMovement: 15,
+			availableElemTypes: [
+				ElemType.Elem1,
+				ElemType.Elem2,
+				ElemType.Elem8,
+				ElemType.Elem9,
+				ElemType.Elem10,
+				ElemType.Elem11
+			],
+			rawMap: [
+				[  0,  0, -2,  0,  0, -2,  0,  0 ],
+				[  0, -2, -2, -2, -2, -2, -2,  0 ],
+				[ -2, -2, -2, -2, -2, -2, -2, -2 ],
+				[ -2, -2, -2, -2, -2, -2, -2, -2 ],
+				[ -2, -2, -2, -2, -2, -2, -2, -2 ],
+				[ -2, -2, -2, -2, -2, -2, -2, -2 ],
+				[ -2, -2, -2, -2, -2, -2, -2, -2 ],
+				[ -2, -2, -2, -2, -2, -2, -2, -2 ]
+			],
+			starRequirements: [
+				3000,
+				5000,
+				6000
+			],
+			elemGoals: [
+				ElemType.Elem2 => 15,
+				ElemType.Elem8 => 5,
+				ElemType.Elem9 => 5,
+				ElemType.Elem10 => 20,
+			]
+		},
+
+		// LV 7 =====================================
+		{
+			maxMovement: 10,
+			availableElemTypes: [
+				ElemType.Elem3,
+				ElemType.Elem8,
+				ElemType.Elem9,
+				ElemType.Elem10,
+				ElemType.Elem11
+			],
+			rawMap: [
+				[ -2, -2, -2, -2, -2, -2, -2, -2 ],
+				[ -2, -2, -2, -2, -2, -2, -2, -2 ],
+				[ -2, -2, -2, -2, -2, -2, -2, -2 ],
+				[ -3, -3, -3, -3, -3, -3, -3, -3 ],
+				[ -2, -2, -2, -2, -2, -2, -2, -2 ],
+				[ -2, -2, -2, -2, -2, -2, -2, -2 ],
+				[ -2, -2, -2, -2, -2, -2, -2, -2 ]
+			],
+			starRequirements: [
+				3000,
+				5000,
+				6000
+			],
+			elemGoals: [
+				ElemType.Elem3 => 10,
+				ElemType.Elem8 => 15,
+				ElemType.Elem11 => 10,
 			]
 		}
 	];
